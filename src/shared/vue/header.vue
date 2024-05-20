243e42f1900941b2b7775b79fab89149c5950cde
Yuuma19946325 <yuuma19946325@gmail.com>
2024-05-21 02:50:28 +0900

    新規作成

---
+++ src/shared/vue/header.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="nav">
+      <div>ロゴ</div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.nav {
+  height: 43px;
+  width: 100%;
+  color: #ffff;
+  background-color: #131921;
+}
+</style>
