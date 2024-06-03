984ba6f276a4fc1c7d3e97dbe5acda78fa16d042
Yuuma19946325 <yuuma19946325@gmail.com>
2024-06-03 18:29:05 +0900

    goodsを作成

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,63 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './pages/components/HelloWorld.vue'
+import { useCounterStore } from "./shared/stores/counter";
+import { useGoodsStore } from "./shared/stores/goods";
+import { Subject } from 'rxjs';
+import { from } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import axios from 'axios';
+
+const counterStore = useCounterStore();
+const goodsStore = useGoodsStore();
+
+const count = computed(
+  (): number => {
+    return counterStore.count;
+  }
+);
+
+const doubleCount = computed(
+  (): number => {
+    return counterStore.doubleCount;
+  }
+);
+
+const onIncrementClick = () => {
+  counterStore.increment();
+};
+
+const onIncrementClick2 = () => {
+  goodsStore.increment();
+};
+
+const fetchData = () => {
+      const apiObservable = from(axios.get('http://localhost:8080/api/goods/list')).pipe(
+        map(response => response.data),
+        catchError(err => {
+          return [];
+        })
+      );
+
+      apiObservable.subscribe({
+        next: result => {
+          console.log(result)
+        },
+        error: err => {
+          console.log(err)
+        }
+      });
+    }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
-
-  <RouterView />
+  <p>count: {{ count }}</p>
+  <p>doubleCount: {{ doubleCount }}</p>
+  <button v-on:click="onIncrementClick2">加算</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/shared/stores/counter.ts
+++ src/shared/stores/counter.ts
@@ -1,12 +1,24 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+interface State {
+	count: number;
+}
 
-  return { count, doubleCount, increment }
-})
+export const useCounterStore = defineStore("counter", {
+	state: (): State => {
+		return {
+			count: 0
+		};
+	},
+	getters: {
+		doubleCount: (state): number => {
+			return state.count * 2;
+		}
+	},
+	actions: {
+		increment(): void {
+			this.count++;
+		}
+	}
+});
