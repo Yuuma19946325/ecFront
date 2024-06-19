f5c3a44f97171b2fc54941acca0b0d1227c15f34
Yuuma19946325 <yuuma19946325@gmail.com>
2024-06-19 18:43:27 +0900

    商品追加

---
+++ src/pages/dialog/GoodsAddDialog.vue
@@ -0,0 +1,200 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useField, useForm } from 'vee-validate';
+import * as yup from 'yup';
+import CategorySelectBox from '@/pages/components/CategorySelectBox.vue';
+
+// ダイアログの開閉状態
+const dialogOpen = ref<boolean>(false);
+
+// ダイアログを開くメソッド
+const open = (): void => {
+    dialogOpen.value = true;
+}
+
+// バリデーションスキーマの定義
+const schema  = yup.object({
+    image: yup.string().required('画像が設定されておりません'),
+    goodsName: yup.string().required('商品名が入力されておりません'),
+    categoryId: yup.string().required('カテゴリが選択されておりません'),
+    amount: yup.string().required('金額が入力されておりません').matches(/^[1-9]+$/, '金額は1以上で入力してください'),
+    stock: yup.string().required('在庫が入力されておりません').matches(/^[1-9]+$/, '在庫は1以上で入力してください'),
+    set: yup.string(),
+    material: yup.string(),
+    brand: yup.string(),
+    theme: yup.string(),
+    target: yup.string(),
+    point: yup.string(),
+});
+
+
+// useFormフックの使用
+const { handleSubmit, errors } = useForm({
+    validationSchema: schema,
+});
+
+// 各フィールドの定義
+const { value: image } = useField('image');
+const { value: goodsName } = useField('goodsName');
+const { value: categoryId } = useField('categoryId');
+const { value: amount } = useField('amount');
+const { value: stock } = useField('stock');
+const { value: set } = useField('set');
+const { value: material } = useField('material');
+const { value: brand } = useField('brand');
+const { value: theme } = useField('theme');
+const { value: target } = useField('target');
+const { value: point } = useField('point');
+
+const file = ref<File | null>(null);
+const URL1 = ref<string | undefined>(undefined);
+const base64 = ref<string | null>(null);
+
+const onFileAdded = (files: any) => {
+    file.value = files[0];
+    URL1.value = URL.createObjectURL(file.value);
+
+    const convertToBase64 = () => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.value!);
+        reader.onload = () => {
+            base64.value = reader.result as string;
+            console.log('base64', base64);
+        };
+    };
+};
+
+// フォーム送信ハンドラー
+const onSubmit = handleSubmit((values) => {
+    console.log('Form submitted', values);
+});
+
+// コンポーネントのメソッドを公開
+defineExpose({
+    open
+});
+</script>
+
+<template>
+    <q-dialog v-model="dialogOpen" persistent>
+        <q-card>
+            <q-card-section class="row items-center q-pb-none">
+                <div class="text-h6">
+                    <label>商品追加</label>
+                </div>
+                <q-space />
+                <q-btn icon="close" flat round dense @click="dialogOpen = false" />
+            </q-card-section>
+            <q-card-section>
+                <q-page>
+                    <div class="vertical-table">
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-uploader
+                                    v-model="file"
+                                    label="Select an image"
+                                    accept="image/*"
+                                    @added="onFileAdded"
+                                />
+                                <div v-if="file">
+                                    <img :src="URL1" alt="Selected Image" style="max-width: 100%; height: auto;">
+                                </div>
+                                {{ base64 }}
+                                <div class="error-message">{{ errors.image }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="goodsName" type="text" id="goodsName" label="商品名" filled dense />
+                                <div class="error-message">{{ errors.goodsName }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <CategorySelectBox @categoryId="categoryId = $event"/>
+                                <div class="error-message">{{ errors.categoryId }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="amount" type="number" id="amount" label="金額" filled dense />
+                                <div class="error-message">{{ errors.amount }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="stock" type="number" id="stock" label="在庫" filled dense />
+                                <div class="error-message">{{ errors.stock }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="set" type="number" id="set" label="セット個数" filled dense />
+                                <div class="error-message">{{ errors.set }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="material" type="text" id="material" label="材質" filled dense />
+                                <div class="error-message">{{ errors.material }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="brand" type="text" id="brand" label="ブランド" filled dense />
+                                <div class="error-message">{{ errors.brand }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="theme" type="text" id="theme" label="テーマ" filled dense />
+                                <div class="error-message">{{ errors.theme }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="target" type="number" id="target" label="対象年齢" filled dense />
+                                <div class="error-message">{{ errors.target }}</div>
+                            </div>
+                        </div>
+                        <div class="vertical-header">
+                            <div class="vertical-cell">
+                                <q-input v-model="point" type="number" id="point" label="付与ポイント" filled dense />
+                                <div class="error-message">{{ errors.point }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </q-page>
+            </q-card-section>
+            <q-card-section class="row justify-end q-pt-none">
+                <q-btn class="action-button" color="primary" label="新規追加" @click="onSubmit()" />
+            </q-card-section>
+        </q-card>
+    </q-dialog>
+</template>
+
+<style scoped>
+.vertical-table {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #ccc;
+}
+
+.vertical-header {
+    display: flex;
+    flex-direction: row;
+}
+
+.vertical-cell {
+    padding: 8px;
+    border: 1px solid #ccc;
+    text-align: left;
+    min-width: 500px;
+}
+
+.error-message {
+    color: red;
+    font-size: 0.75rem;
+    margin-top: 4px; /* エラーメッセージとの間隔を調整 */
+}
+</style>
